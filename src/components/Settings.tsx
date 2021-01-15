423e869ee3ec37afb383d0c514fc834e62cc2ffc
TheodoreChu <22967798+TheodoreChu@users.noreply.github.com>
2021-01-14 18:52:52 -0800

    feat: titles for settings

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -71,6 +71,7 @@ export default class Settings extends React.Component<
               className={'sk-button button sk-secondary-contrast'}
               id={HtmlElementId.settingsToggleButton}
               onClick={this.props.toggleShowSettings}
+              title={'Show Settings'}
             >
               <ChevronIconRight role={'button'} />
             </button>
@@ -80,6 +81,7 @@ export default class Settings extends React.Component<
               className={'sk-button button sk-secondary-contrast'}
               id={HtmlElementId.settingsToggleButton}
               onClick={this.props.toggleShowSettings}
+              title={'Hide Settings'}
             >
               <ChevronIconDown role={'button'} />
             </button>
@@ -87,97 +89,107 @@ export default class Settings extends React.Component<
         </section>
         {this.props.showSettings && [
           <section className="settings language">
-            <label htmlFor={HtmlElementId.language}>Language:&nbsp;</label>
+            <label
+              htmlFor={HtmlElementId.language}
+              title={'Programming language. Default is Markdown'}
+            >
+              Language:&nbsp;
+            </label>
             <div>
-              <label>
-                <select
-                  id={HtmlElementId.language}
-                  name={HtmlElementId.language}
-                  value={this.props.language}
-                  onChange={this.props.handleSelectChange}
-                >
-                  <option>abap</option>
-                  <option>aes</option>
-                  <option>apex</option>
-                  <option>azcli</option>
-                  <option>bat</option>
-                  <option>c</option>
-                  <option>cameligo</option>
-                  <option>clojure</option>
-                  <option>coffeescript</option>
-                  <option>cpp</option>
-                  <option>csharp</option>
-                  <option>csp</option>
-                  <option>css</option>
-                  <option>dart</option>
-                  <option>dockerfile</option>
-                  <option>fsharp</option>
-                  <option>go</option>
-                  <option>graphql</option>
-                  <option>handlebars</option>
-                  <option>hcl</option>
-                  <option>html</option>
-                  <option>ini</option>
-                  <option>java</option>
-                  <option>javascript</option>
-                  <option>json</option>
-                  <option>julia</option>
-                  <option>kotlin</option>
-                  <option>less</option>
-                  <option>lexon</option>
-                  <option>lua</option>
-                  <option>markdown</option>
-                  <option>mips</option>
-                  <option>msdax</option>
-                  <option>mysql</option>
-                  <option>objective-c</option>
-                  <option>pascal</option>
-                  <option>pascaligo</option>
-                  <option>perl</option>
-                  <option>pgsql</option>
-                  <option>php</option>
-                  <option>plaintext</option>
-                  <option>postiats</option>
-                  <option>powerquery</option>
-                  <option>powershell</option>
-                  <option>pug</option>
-                  <option>python</option>
-                  <option>r</option>
-                  <option>razor</option>
-                  <option>redis</option>
-                  <option>redshift</option>
-                  <option>restructuredtext</option>
-                  <option>ruby</option>
-                  <option>rust</option>
-                  <option>sb</option>
-                  <option>scala</option>
-                  <option>scheme</option>
-                  <option>scss</option>
-                  <option>shell</option>
-                  <option>sol</option>
-                  <option>sql</option>
-                  <option>st</option>
-                  <option>swift</option>
-                  <option>systemverilog</option>
-                  <option>tcl</option>
-                  <option>twig</option>
-                  <option>typescript</option>
-                  <option>vb</option>
-                  <option>verilog</option>
-                  <option>xml</option>
-                  <option>yaml</option>
-                </select>
-              </label>
+              <select
+                id={HtmlElementId.language}
+                name={HtmlElementId.language}
+                onChange={this.props.handleSelectChange}
+                title={'Programming language. Default is Markdown'}
+                value={this.props.language}
+              >
+                <option>abap</option>
+                <option>aes</option>
+                <option>apex</option>
+                <option>azcli</option>
+                <option>bat</option>
+                <option>c</option>
+                <option>cameligo</option>
+                <option>clojure</option>
+                <option>coffeescript</option>
+                <option>cpp</option>
+                <option>csharp</option>
+                <option>csp</option>
+                <option>css</option>
+                <option>dart</option>
+                <option>dockerfile</option>
+                <option>fsharp</option>
+                <option>go</option>
+                <option>graphql</option>
+                <option>handlebars</option>
+                <option>hcl</option>
+                <option>html</option>
+                <option>ini</option>
+                <option>java</option>
+                <option>javascript</option>
+                <option>json</option>
+                <option>julia</option>
+                <option>kotlin</option>
+                <option>less</option>
+                <option>lexon</option>
+                <option>lua</option>
+                <option>markdown</option>
+                <option>mips</option>
+                <option>msdax</option>
+                <option>mysql</option>
+                <option>objective-c</option>
+                <option>pascal</option>
+                <option>pascaligo</option>
+                <option>perl</option>
+                <option>pgsql</option>
+                <option>php</option>
+                <option>plaintext</option>
+                <option>postiats</option>
+                <option>powerquery</option>
+                <option>powershell</option>
+                <option>pug</option>
+                <option>python</option>
+                <option>r</option>
+                <option>razor</option>
+                <option>redis</option>
+                <option>redshift</option>
+                <option>restructuredtext</option>
+                <option>ruby</option>
+                <option>rust</option>
+                <option>sb</option>
+                <option>scala</option>
+                <option>scheme</option>
+                <option>scss</option>
+                <option>shell</option>
+                <option>sol</option>
+                <option>sql</option>
+                <option>st</option>
+                <option>swift</option>
+                <option>systemverilog</option>
+                <option>tcl</option>
+                <option>twig</option>
+                <option>typescript</option>
+                <option>vb</option>
+                <option>verilog</option>
+                <option>xml</option>
+                <option>yaml</option>
+              </select>
             </div>
           </section>,
           <section className="settings">
-            <label htmlFor={HtmlElementId.fontSize}>Font size:&nbsp;</label>
-            <div className="input-and-undo-button">
+            <label
+              htmlFor={HtmlElementId.fontSize}
+              title={'Font size in pixels. Default is 16px (12pt)'}
+            >
+              Font size:&nbsp;
+            </label>
+            <div className="settings-input">
               <select
                 id={HtmlElementId.fontSize}
                 name={HtmlElementId.fontSize}
-                value={this.props.fontSize}
                 onChange={this.props.handleSelectChange}
+                title={'Font size in pixels. Default is 16px (12pt)'}
+                value={this.props.fontSize}
               >
                 <option>12px</option>
                 <option>13px</option>
@@ -198,13 +210,23 @@ export default class Settings extends React.Component<
             </div>
           </section>,
           <section className="settings">
-            <label htmlFor={HtmlElementId.tabSize}>Tab size:&nbsp;</label>
-            <div className="input-and-undo-button">
+            <label
+              htmlFor={HtmlElementId.tabSize}
+              title={
+                'Number of spaces inserted when you press Tab. Default is 2'
+              }
+            >
+              Tab size:&nbsp;
+            </label>
+            <div className="settings-input">
               <select
                 id={HtmlElementId.tabSize}
                 name={HtmlElementId.tabSize}
-                value={this.props.tabSize}
                 onChange={this.props.handleSelectChange}
+                title={
+                  'Number of spaces inserted when you press Tab. Default is 2'
+                }
+                value={this.props.tabSize}
               >
                 <option>2</option>
                 <option>4</option>
@@ -212,27 +234,39 @@ export default class Settings extends React.Component<
             </div>
           </section>,
           <section className="settings">
-            <label htmlFor={HtmlElementId.minimap}>Minimap:&nbsp;</label>
-            <div className="input-and-undo-button">
+            <label
+              htmlFor={HtmlElementId.minimap}
+              title={'Toggle show minimap. Default is on (checked)'}
+            >
+              Minimap:&nbsp;
+            </label>
+            <div className="settings-input">
               <label className={'checkbox-input-label'}>
                 <input
+                  checked={this.props.minimap}
                   id={HtmlElementId.minimap}
                   name={HtmlElementId.minimap}
+                  title={'Toggle show minimap. Default is on (checked)'}
                   type="checkbox"
-                  checked={this.props.minimap}
                   onChange={this.props.handleInputChange}
                 />
               </label>
             </div>
           </section>,
           <section className="settings">
-            <label htmlFor={HtmlElementId.theme}>Theme:&nbsp;</label>
-            <div className="input-and-undo-button">
+            <label
+              htmlFor={HtmlElementId.theme}
+              title={'Theme for your code editor. Default is sn-theme'}
+            >
+              Theme:&nbsp;
+            </label>
+            <div className="settings-input">
               <select
                 id={HtmlElementId.theme}
                 name={HtmlElementId.theme}
-                value={this.props.theme}
                 onChange={this.props.handleSelectChange}
+                title={'Theme for your code editor. Default is sn-theme'}
+                value={this.props.theme}
               >
                 <option>vs</option>
                 <option>vs-dark</option>
@@ -242,13 +276,19 @@ export default class Settings extends React.Component<
             </div>
           </section>,
           <section className="settings">
-            <label htmlFor={HtmlElementId.wordWrap}>Word wrap:&nbsp;</label>
-            <div className="input-and-undo-button">
+            <label
+              htmlFor={HtmlElementId.wordWrap}
+              title={'Option to wrap long lines of text. Default is on'}
+            >
+              Word wrap:&nbsp;
+            </label>
+            <div className="settings-input">
               <select
                 id={HtmlElementId.wordWrap}
                 name={HtmlElementId.wordWrap}
-                value={this.props.wordWrap}
                 onChange={this.props.handleSelectChange}
+                title={'Option to wrap long lines of text. Default is on'}
+                value={this.props.wordWrap}
               >
                 <option>on</option>
                 <option>off</option>
@@ -262,14 +302,14 @@ export default class Settings extends React.Component<
             <button
               className={'sk-button button sk-secondary-contrast'}
               onClick={this.props.saveDefaultSettings}
-              title="Save current settings as your default"
+              title="Save current settings as your personal default"
             >
               <SaveIcon role={'button'} />
             </button>,
             <button
               className={'sk-button button sk-secondary-contrast'}
               onClick={this.loadDefaultSettings}
-              title="Load your default settings"
+              title="Load your personal default settings"
             >
               <RefreshIcon role={'button'} />
             </button>,
@@ -277,7 +317,7 @@ export default class Settings extends React.Component<
           <button
             className={'sk-button button sk-secondary-contrast'}
             onClick={this.props.refreshEditor}
-            title="Resize the editor"
+            title="Refresh and resize the editor"
           >
             <WindowIcon role={'button'} />
           </button>
